9e9ee27f26a15c276e29170df04fca64e10bf860
aliaa.a2 <aliaa.a2@P-0G-0B005-0395>
2022-08-31 12:31:27 +0200

    Projektdateien hinzufügen.

---
+++ Datentyp Csharp/Program.cs
@@ -0,0 +1,24 @@
+﻿
+Console.WriteLine("Hello, World!");
+
+string vorname = "Aliaa";
+short alter = 17;
+bool antwort = false;
+
+Console.WriteLine($"mein name ist {vorname}Ich bin { alter}");
+
+
+   
+int DollarZahl = 36;
+var newVariableDollarZahl = (char)DollarZahl;
+Console.WriteLine(newVariableDollarZahl);
+
+
+
+Console.WriteLine ("Geben sie eine Zahl im Wertebereich von short ein");
+short short_Number = Convert.ToInt16(Console.ReadLine);
+Console.WriteLine("Geben sie auch noch eine Zahl im Wertebereich von double ein");
+double double_Number = Convert.ToInt64(Console.ReadLine);
+Console.WriteLine(double_Number);
+Console.WriteLine(short_Number);
+
